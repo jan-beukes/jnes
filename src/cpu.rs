6388e69875872be8f99cd017a3b5b475cb6b79a3
Jan Beukes <beukes.janadam@gmail.com>
2025-04-27 23:20:06 +0200

    some more ops

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,12 +1,33 @@
 use std::collections::HashMap;
 
+use bitflags::bitflags;
+use lazy_static::lazy_static;
+
+// status flags
+bitflags! {
+    pub struct CpuFlags: u8 {
+        const CARRY             = 0b00000001;
+        const ZERO              = 0b00000010;
+        const INTERRUPT_DISABLE = 0b00000100;
+        const DECIMAL_MODE      = 0b00001000;
+        const BREAK             = 0b00010000;
+        const BREAK2            = 0b00100000;
+        const OVERFLOW          = 0b01000000;
+        const NEGATIVE          = 0b10000000;
+    }
+}
+
+const STACK: u16 = 0x0100; // stack address
+const STACK_RESET: u8 = 0xFD;
+
 pub struct CPU {
-    register_a: u8,
-    register_x: u8,
-    register_y: u8,
-    status: u8,
-    program_counter: u16,
-    memory: [u8; 0xFFFF],
+    pub register_a: u8,
+    pub register_x: u8,
+    pub register_y: u8,
+    pub status: CpuFlags,
+    pub program_counter: u16,
+    pub stack_pointer: u8,
+    memory: [u8; 0xffff],
 }
 
 #[derive(Debug)]
@@ -23,25 +44,10 @@ pub enum AddressingMode {
     NoneAddressing,
 }
 
-impl CPU {
-    pub fn new() -> Self {
-        CPU {
-            register_a: 0,
-            register_x: 0,
-            register_y: 0,
-            status: 0,
-            program_counter: 0,
-            memory: [0; 0xFFFF],
-        }
-    }
+trait Mem {
+    fn mem_read(&self, addr: u16) -> u8;
 
-    fn mem_read(&self, addr: u16) -> u8 {
-        self.memory[addr as usize]
-    }
-
-    fn mem_write(&mut self, addr: u16, data: u8) {
-        self.memory[addr as usize] = data;
-    }
+    fn mem_write(&mut self, addr: u16, data: u8);
 
     fn mem_read_u16(&mut self, pos: u16) -> u16 {
         let lo = self.mem_read(pos) as u16;
@@ -56,6 +62,30 @@ impl CPU {
         self.mem_write(pos, lo);
         self.mem_write(pos + 1, hi);
     }
+}
+
+impl Mem for CPU {
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+}
+
+impl CPU {
+    pub fn new() -> Self {
+        CPU {
+            register_a: 0,
+            register_x: 0,
+            register_y: 0,
+            stack_pointer: STACK_RESET,
+            program_counter: 0,
+            status: CpuFlags::from_bits_truncate(0b100100),
+            memory: [0; 0xffff],
+        }
+    }
 
     pub fn load(&mut self, program: Vec<u8>) {
         // memcpy program into memory
@@ -68,7 +98,8 @@ impl CPU {
         self.register_a = 0;
         self.register_x = 0;
         self.register_y = 0;
-        self.status = 0;
+        self.stack_pointer = STACK_RESET;
+        self.status = CpuFlags::from_bits_truncate(0b100100);
 
         // set program counter to saved address of program
         self.program_counter = self.mem_read_u16(0xFFFC);
@@ -91,11 +122,17 @@ impl CPU {
                 .expect(&format!("OpCode {:x} is not recognised!", code));
 
             match code {
-                //LDA
+                // ADC
+                0x69 | 0x65 | 0x75 | 0x6d | 0x7d | 0x79 | 0x61 | 0x71 => {
+                    self.adc(&opcode.mode);
+                }
+                0x29 | 0x25 | 0x35 | 0x2d | 0x3d | 0x39 | 0x21 | 0x31 => {
+                    self.and(&opcode.mode);
+                }
+                // LDA
                 0xA9 | 0xA5 | 0xAD | 0xBD | 0xB9 | 0xA1 | 0xB1 => {
                     self.lda(&opcode.mode);
                 }
-
                 // STA
                 0x85 | 0x9A | 0x8D | 0x9D | 0x99 | 0x81 | 0x91 => {
                     self.sta(&opcode.mode);
@@ -116,6 +153,47 @@ impl CPU {
     /***************
      * CPU OPS
      * *************/
+
+    fn add_to_register_a(&mut self, data: u8) {
+        let carry: u16 = if self.status.contains(CpuFlags::CARRY) {
+            1
+        } else {
+            0
+        };
+        let sum = self.register_a as u16 + data as u16 + carry;
+
+        // carry
+        if sum > 0xff {
+            self.status.insert(CpuFlags::CARRY);
+        } else {
+            self.status.remove(CpuFlags::CARRY);
+        }
+
+        let result = sum as u8;
+        // check for overflow of signed number
+        if (data ^ result) & (result ^ self.register_a) & 0x80 != 0 {
+            self.status.insert(CpuFlags::OVERFLOW);
+        } else {
+            self.status.remove(CpuFlags::OVERFLOW);
+        }
+        self.register_a = result;
+        self.update_zero_and_negative_flags(result);
+    }
+
+    // add to a
+    fn adc(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+        self.add_to_register_a(value);
+    }
+
+    // and a and value
+    fn and(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+        self.register_a &= value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
 
     // load into a
     fn lda(&mut self, mode: &AddressingMode) {
@@ -200,15 +278,15 @@ impl CPU {
     // set the the status bits
     fn update_zero_and_negative_flags(&mut self, result: u8) {
         if result == 0 {
-            self.status |= 0b0000_0010;
+            self.status.insert(CpuFlags::ZERO);
         } else {
-            self.status &= 0b1111_1101;
+            self.status.remove(CpuFlags::ZERO);
         }
 
         if result & 0b1000_0000 != 0 {
-            self.status |= 0b1000_0000;
+            self.status.insert(CpuFlags::NEGATIVE);
         } else {
-            self.status &= 0b0111_1111;
+            self.status.remove(CpuFlags::NEGATIVE);
         }
     }
 }
@@ -242,12 +320,32 @@ impl OpCode {
 //---OpCodes---
 //
 // static initialization at runtime
-lazy_static::lazy_static! {
+lazy_static! {
     static ref CPU_OPCODES: Vec<OpCode> = vec![
         OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing),
         OpCode::new(0xaa, "TAX", 1, 2, AddressingMode::NoneAddressing),
         OpCode::new(0xe8, "INX", 1, 2, AddressingMode::NoneAddressing),
+        // AND
+        OpCode::new(0x29, "AND", 2, 2, AddressingMode::Immediate),
+        OpCode::new(0x25, "AND", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0x35, "AND", 2, 4, AddressingMode::ZeroPageX),
+        OpCode::new(0x2d, "AND", 3, 4, AddressingMode::Absolute),
+        OpCode::new(0x3d, "AND", 3, 4, AddressingMode::AbsoluteX),
+        OpCode::new(0x39, "AND", 3, 4, AddressingMode::AbsoluteY),
+        OpCode::new(0x21, "AND", 2, 6, AddressingMode::IndirectX),
+        OpCode::new(0x31, "AND", 2, 5, AddressingMode::IndirectY),
 
+        // ADC
+        OpCode::new(0x69, "ADC", 2, 2, AddressingMode::Immediate),
+        OpCode::new(0x65, "ADC", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0x75, "ADC", 2, 4, AddressingMode::ZeroPageX),
+        OpCode::new(0x6d, "ADC", 3, 4, AddressingMode::Absolute),
+        OpCode::new(0x7d, "ADC", 3, 4, AddressingMode::AbsoluteX),
+        OpCode::new(0x79, "ADC", 3, 4, AddressingMode::AbsoluteY),
+        OpCode::new(0x61, "ADC", 2, 6, AddressingMode::IndirectX),
+        OpCode::new(0x71, "ADC", 2, 5, AddressingMode::IndirectY),
+
+        // LDA
         OpCode::new(0xa9, "LDA", 2, 2, AddressingMode::Immediate),
         OpCode::new(0xa5, "LDA", 2, 3, AddressingMode::ZeroPage),
         OpCode::new(0xb5, "LDA", 2, 4, AddressingMode::ZeroPageX),
@@ -256,7 +354,7 @@ lazy_static::lazy_static! {
         OpCode::new(0xb9, "LDA", 3, 4, AddressingMode::AbsoluteY),
         OpCode::new(0xa1, "LDA", 2, 6, AddressingMode::IndirectX),
         OpCode::new(0xb1, "LDA", 2, 5, AddressingMode::IndirectY),
-
+        // STA
         OpCode::new(0x85, "STA", 2, 3, AddressingMode::ZeroPage),
         OpCode::new(0x95, "STA", 2, 4, AddressingMode::ZeroPageX),
         OpCode::new(0x8d, "STA", 3, 4, AddressingMode::Absolute),
@@ -264,9 +362,7 @@ lazy_static::lazy_static! {
         OpCode::new(0x99, "STA", 3, 5, AddressingMode::AbsoluteY),
         OpCode::new(0x81, "STA", 2, 6, AddressingMode::IndirectX),
         OpCode::new(0x91, "STA", 2, 6, AddressingMode::IndirectY),
-
     ];
-
     static ref OPCODES_MAP: HashMap<u8, &'static OpCode> = {
         let mut map = HashMap::new();
         for cpuop in &*CPU_OPCODES {
@@ -286,15 +382,15 @@ mod tests {
         // lda with arg 0x05 and then break
         cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
         assert_eq!(cpu.register_a, 0x05);
-        assert!(cpu.status & 0b0000_0010 == 0);
-        assert!(cpu.status & 0b1000_0000 == 0);
+        assert!(!cpu.status.contains(CpuFlags::ZERO));
+        assert!(!cpu.status.contains(CpuFlags::NEGATIVE));
     }
 
     #[test]
     fn test_lda_zero_flag() {
         let mut cpu = CPU::new();
         cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
-        assert!(cpu.status & 0b0000_0010 == 0b10);
+        assert!(cpu.status.contains(CpuFlags::ZERO));
     }
 
     #[test]
